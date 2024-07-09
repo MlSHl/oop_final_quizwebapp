0a7a66889b36cbd87d1af11cb9f8e63afc75f47f
DavitBarnabishvili <dbarn21@freeuni.edu.ge>
2024-07-09 19:09:04 +0400

    search Implemented

--- src/main/java/org/example/quizwebapp/DAO/QuizDAO.java
+++ src/main/java/org/example/quizwebapp/DAO/QuizDAO.java
@@ -135,4 +135,122 @@ public class QuizDAO {
         return quizzes;
     }
 
+    public List<Quiz> getQuizByQuizName(String name) throws SQLException, ClassNotFoundException {
+        List<Quiz> quizzes = new ArrayList<>();
+        String quizSql = "SELECT quiz_id, quiz_name, quiz_desc FROM quizzes WHERE quiz_name = ?";
+        String questionSql = "SELECT question_id, question_text FROM questions WHERE quiz_id = ?";
+        String answerSql = "SELECT answer_text, answer_type FROM answers WHERE question_id = ?";
+
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        Connection connection = connectionPool.getConnection();
+
+        try (PreparedStatement quizStatement = connection.prepareStatement(quizSql)) {
+            quizStatement.setString(1, name);
+            ResultSet quizResultSet = quizStatement.executeQuery();
+
+            while (quizResultSet.next()) {
+                int quizId = quizResultSet.getInt("quiz_id");
+                String quizName = quizResultSet.getString("quiz_name");
+                String quizDesc = quizResultSet.getString("quiz_desc");
+
+                Quiz quiz = new Quiz(quizName, quizDesc, new ArrayList<>());
+                quiz.setId(quizId);
+
+                try (PreparedStatement questionStatement = connection.prepareStatement(questionSql)) {
+                    questionStatement.setInt(1, quizId);
+                    ResultSet questionResultSet = questionStatement.executeQuery();
+
+                    while (questionResultSet.next()) {
+                        int questionId = questionResultSet.getInt("question_id");
+                        String questionText = questionResultSet.getString("question_text");
+
+                        Question question = new Question(questionText);
+                        question.setQuestionId(questionId);
+                        quiz.getQuestions().add(question);
+
+                        // Fetch answers for the current question
+                        try (PreparedStatement answerStatement = connection.prepareStatement(answerSql)) {
+                            answerStatement.setInt(1, questionId);
+                            ResultSet answerResultSet = answerStatement.executeQuery();
+
+                            while (answerResultSet.next()) {
+                                String answerText = answerResultSet.getString("answer_text");
+                                String answerType = answerResultSet.getString("answer_type");
+
+                                boolean isCorrect = "C".equals(answerType);
+                                question.addAnswer(answerText, isCorrect);
+                            }
+                        }
+                    }
+                }
+
+                quizzes.add(quiz);
+            }
+        } finally {
+            connection.close();
+        }
+
+        return quizzes;
+    }
+
+
+
+    public List<Quiz> getQuizByQuizDesc(String desc) throws SQLException, ClassNotFoundException {
+        List<Quiz> quizzes = new ArrayList<>();
+        String quizSql = "SELECT quiz_id, quiz_name, quiz_desc FROM quizzes WHERE quiz_desc = ?";
+        String questionSql = "SELECT question_id, question_text FROM questions WHERE quiz_id = ?";
+        String answerSql = "SELECT answer_text, answer_type FROM answers WHERE question_id = ?";
+
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        Connection connection = connectionPool.getConnection();
+
+        try (PreparedStatement quizStatement = connection.prepareStatement(quizSql)) {
+            quizStatement.setString(1, desc);
+            ResultSet quizResultSet = quizStatement.executeQuery();
+
+            while (quizResultSet.next()) {
+                int quizId = quizResultSet.getInt("quiz_id");
+                String quizName = quizResultSet.getString("quiz_name");
+                String quizDesc = quizResultSet.getString("quiz_desc");
+
+                Quiz quiz = new Quiz(quizName, quizDesc, new ArrayList<>());
+                quiz.setId(quizId);
+
+                try (PreparedStatement questionStatement = connection.prepareStatement(questionSql)) {
+                    questionStatement.setInt(1, quizId);
+                    ResultSet questionResultSet = questionStatement.executeQuery();
+
+                    while (questionResultSet.next()) {
+                        int questionId = questionResultSet.getInt("question_id");
+                        String questionText = questionResultSet.getString("question_text");
+
+                        Question question = new Question(questionText);
+                        question.setQuestionId(questionId);
+                        quiz.getQuestions().add(question);
+
+                        try (PreparedStatement answerStatement = connection.prepareStatement(answerSql)) {
+                            answerStatement.setInt(1, questionId);
+                            ResultSet answerResultSet = answerStatement.executeQuery();
+
+                            while (answerResultSet.next()) {
+                                String answerText = answerResultSet.getString("answer_text");
+                                String answerType = answerResultSet.getString("answer_type");
+
+                                boolean isCorrect = "C".equals(answerType);
+                                question.addAnswer(answerText, isCorrect);
+                            }
+                        }
+                    }
+                }
+
+                quizzes.add(quiz);
+            }
+        } finally {
+            connection.close();
+        }
+
+        return quizzes;
+    }
+
+
 }
---
+++ src/main/java/org/example/quizwebapp/DAO/SearchDAO.java
@@ -0,0 +1,85 @@
+package org.example.quizwebapp.DAO;
+
+import org.example.quizwebapp.CustomExceptions.UserNotFoundException;
+import org.example.quizwebapp.Model.Quiz;
+import org.example.quizwebapp.Model.User;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchDAO {
+    private final UserDAO userDao = new UserDAO();
+    private final QuizDAO quizDao = new QuizDAO();
+    public List<User> getUsers(String searchTerm) throws SQLException, ClassNotFoundException {
+        ArrayList<User> usersList = new ArrayList<>();
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        Connection connection = connectionPool.getConnection();
+
+        String sql = "SELECT * FROM users WHERE user_name LIKE ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, "%" + searchTerm + "%");
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String username = resultSet.getString("username");
+
+                User user = userDao.getUserByUsername(username);
+                usersList.add(user);
+            }
+        } catch (UserNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            connection.close();
+        }
+
+        return usersList;
+    }
+
+    public List<Quiz> getQuizzesByName(String searchTerm) throws SQLException, ClassNotFoundException {
+        ArrayList<Quiz> quizList = new ArrayList<>();
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        Connection connection = connectionPool.getConnection();
+
+        String sql = "SELECT * FROM quizzes WHERE quiz_name LIKE ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, "%" + searchTerm + "%");
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("quiz_name");
+
+                List<Quiz> quizMatchName = quizDao.getQuizByQuizName(name);
+                quizList.addAll(quizMatchName);
+            }
+        } finally {
+            connection.close();
+        }
+
+        return quizList;
+    }
+
+    public List<Quiz> getQuizzesByDesc(String searchTerm) throws SQLException, ClassNotFoundException {
+        ArrayList<Quiz> quizList = new ArrayList<>();
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        Connection connection = connectionPool.getConnection();
+
+        String sql = "SELECT * FROM quizzes WHERE quiz_desc LIKE ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, "%" + searchTerm + "%");
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String desc = resultSet.getString("quiz_desc");
+
+                List<Quiz> quizMatchDesc = quizDao.getQuizByQuizDesc(desc);
+                quizList.addAll(quizMatchDesc);
+            }
+        } finally {
+            connection.close();
+        }
+        return quizList;
+    }
+}
